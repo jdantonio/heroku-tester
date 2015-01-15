07c412e6980391bac05fe86e06517cfb44e4536b
Jerry D'Antonio <stumpjumper@gmail.com>
2015-01-15 13:08:35 -0500

    Testing JRuby

--- hello.rb
+++ hello.rb
@@ -39,7 +39,14 @@ get '/' do
 
   begin
     require 'concurrent/extension'
-    response += "<tr><td>concurrent/extension</td><td>loaded</td></tr>"
+    response += "<tr><td>C extensions</td><td>loaded</td></tr>"
+  rescue LoadError => ex
+    response += "<tr><td>#{ex.class}</td><td>#{ex.message}</td></tr>"
+  end
+
+  begin
+    require 'concurrent_ruby_ext'
+    response += "<tr><td>Java extensions</td><td>loaded</td></tr>"
   rescue LoadError => ex
     response += "<tr><td>#{ex.class}</td><td>#{ex.message}</td></tr>"
   end
